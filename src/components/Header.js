90a099db67cce4bee50e06b7bda74668f7122197
rebeljeet <jeetpatel7879@gmail.com>
2021-05-28 00:34:37 +0530

    add Header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,65 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
-        <div>
-            header
-        </div>
+        <Container>
+            <a>
+                <img src="/images/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+            </RightMenu>
+
+            <CustomMenu>
+                
+            </CustomMenu>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    dispaly: flex;
+    align-items: center;
+    padding: 0px 20px;
+    top:0;
+    left:0;
+    right:0;
+    
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    flex:1;
+
+    a{
+        font-weight:600;
+        text-transform:uppercase;
+        padding: 0 10px;
+        flex-wrap: no-wrap;
+    }
+`
+const RightMenu = styled.div`
+    a{
+        font-weight:600;
+        text-transform:uppercase;
+        margin-right: 10px;
+    }
+`
+const CustomMenu = styled(MenuIcon)`
+
+`
