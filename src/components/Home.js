990e98b26e4200a0b3220a312634c899fade5391
rebeljeet <jeetpatel7879@gmail.com>
2021-05-27 23:14:13 +0530

    ADD: add two another section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,35 +5,55 @@ import Section from "./Section"
 function Home() {
     return (
         <container>
-            <Section 
+            <Section
                 title="Model S"
                 description="Order Online for Touchless delivery"
                 backgroundImg="model-s.jpg"
                 leftBtnText="Custom Order"
                 rightBtnText="Existing inventory"
             />
-            <Section 
+            <Section
                 title="Model Y"
                 description="Order Online for Touchless delivery"
                 backgroundImg="model-y.jpg"
                 leftBtnText="Custom Order"
                 rightBtnText="Existing inventory"
             />
-            <Section 
+            <Section
                 title="Model 3"
                 description="Order Online for Touchless delivery"
                 backgroundImg="model-3.jpg"
                 leftBtnText="Custom Order"
                 rightBtnText="Existing inventory"
             />
-            <Section 
+            <Section
                 title="Model X"
                 description="Order Online for Touchless delivery"
                 backgroundImg="model-x.jpg"
                 leftBtnText="Custom Order"
                 rightBtnText="Existing inventory"
             />
-            
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money-back Guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Solar for New Roofs"
+                description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop Now"
+                //rightBtnText=""
+            />
         </container>
     )
 }
